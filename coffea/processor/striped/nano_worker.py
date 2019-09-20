e57ab2f2bb0bc822ea1295b4958e3136bdb20ca7
Allie Hall <ahall@fnal.gov>
2019-09-20 12:53:21 -0500

    Not accumulating yet

--- coffea/processor/striped/nano_worker.py
+++ coffea/processor/striped/nano_worker.py
@@ -1,20 +1,21 @@
 import numpy as np
+import cloudpickle as cpkl
+import pickle as pkl
+from coffea import processor
+import lz4.frame as lz4f
 
 class Worker(object):
 
-    Columns = ["Jet.pt", "Jet.eta", "Jet.jetId"]
+    Columns = ["Jet.pt", "Jet.eta", "Jet.jetId","MET_pt","Electron.pt"]
 
     def __init__(self, params, bulk, job, database):
         self.Job = job
         self.Params = params
         self.Bulk = bulk
         
-        compressed_processor = bulk["processor"]
-        # XX uncompress processor XX Not sure if this should be here or done per frame. Probably here.
-        self.processor_instance = compressed_processor.XX_uncompress_XX
-        
-        self.out = processor_instance.accumulator.identity()
-        
+        compressed_proc_class = bulk["processor"]
+        self.processor_instance = cpkl.loads(compressed_proc_class)
+        self.out = self.processor_instance.accumulator.identity()   
         self.First = True
         
     def frame(self, events):
@@ -22,22 +23,28 @@ class Worker(object):
         if self.First:
             self.Job.message("Starting dataset")
             self.First = False
-        
         #XX this map will take a lot of the effort in striped to undo what was done during uploading XX
         # XX Once we abstract this away, then the worker class and accumulator can be the same for all processors XX#
         df_items = {}
         df_items['Jet_pt'] = events.Jet.pt
         df_items['Jet_eta'] = events.Jet.eta
-        
-        # XX Need first argument to be the number of events. Need a way to get this generically XX
-        df = processor.PreloadedDataFrame(events.Met_pt.counts, items=df_items)
+        df_items['MET_pt'] = events.MET_pt
 
-        self.processor_instance.process(df)
-            
+        # XX Need first argument to be the number of events. Need a way to get this generically XX
+        df = processor.PreloadedDataFrame(events.MET_pt.size, items=df_items)
+
+        self.out += self.processor_instance.process(df)
+
+#        for k in self.out:
+#            self.Job.message("Output key = %s"%k)
+#            self.Job.message(str(self.out[k].integrate('dataset').values(overflow='all')))
+#            self.Job.message("Done processing")
         #XX Could also accumulate here by returning self.out as done below. Can try both to see if it is faster.
         
     def end(self):
-        return { "coffea_accumulator:" :   self.out    }
+        for k in self.out:
+            self.Job.message(str(self.out[k].integrate('dataset').values(overflow='all')))
+        return { "coffea_accumulator:" : lz4f.compress(pkl.dumps(self.out),compression_level=1)}
         
 class Accumulator:
     
@@ -49,7 +56,13 @@ class Accumulator:
             self.Data = data.copy()
         else:
             for k, v in data.items():
-                self.Data[k] += v
+                self.Data[k] =  zipadd(self.Data[k],v)
+#                self.Data[k] += v
         
     def values(self):
         return self.Data
+
+    def zipadd(a, b):
+        out = pickle.loads(lz4f.decompress(a))
+        out += pickle.loads(lz4f.decompress(b))
+        return lz4f.compress(pickle.dumps(out), compression_level=1)
--- coffea/processor/striped/striped_executor.py
+++ coffea/processor/striped/striped_executor.py
@@ -1,39 +1,80 @@
 import os
 from striped.job import Session
 import numpy as np
+from tqdm import tqdm
+import cloudpickle as cpkl
+import pickle as pkl
+import lz4.frame as lz4f
+from coffea import hist, processor
+
+class AnalysisProcessor(processor.ProcessorABC):
+    def __init__(self):
+        self._accumulator = processor.dict_accumulator({
+                'fj1pt': hist.Hist("Events", hist.Cat("dataset", "Primary dataset"), hist.Bin("fj1pt","AK15 Leading Jet Pt",[200.0, 250.0, 280.0, 310.0, 340.0, 370.0, 400.0, 430.0, 470.0, 510.0, 550.0, 590.0, 640.0, 690.0, 740.0, 790.0, 840.0, 900.0, 960.0, 1020.0, 1090.0, 1160.0, 1250.0]))
+                })
+
+    @property
+    def accumulator(self):
+        return self._accumulator
+
+    def process(self, df):
+        hout = self.accumulator.identity()
+        hout['fj1pt'].fill(dataset="Test",fj1pt=df["Jet_pt"].max().flatten())        
+        return hout
+
+    def postprocess(self, accumulator):
+        return accumulator
+
 
 class CoffeaDataCallback:
     
-    def __init__(self):
-        self.coffea_accumulator = {}
+    def __init__(self, temp_acc):
+        self.coffea_accumulator = lz4f.compress(pkl.dumps(temp_acc),compression_level=1)
+        self.count = 0
     
     def on_data(self, wid, nevents, data):
-        self.coffea_accumulator += data["coffea_accumulator"]
-    
+        out =  pkl.loads(lz4f.decompress(self.coffea_accumulator))
+        out += pkl.loads(lz4f.decompress(data["coffea_accumulator"]))
+        self.coffea_accumulator = lz4f.compress(pkl.dumps(out),compression_level=1)
+        self.count += 1
+
     def on_job_finish(self, nsamples, error):
-        self.coffea_accumulator += data["coffea_accumulator"]
+        nsamples = nsamples
+#        self.coffea_accumulator += data["coffea_accumulator"]
 
 def run_striped_job(datasets, session_name, proc_instance):
-    data_collector = CoffeaDataCallback()
     session = Session(session_name)
-    
-    # XX compress proc_instance to compressed_proc_instance
-    
+        
+    processor_instance = cpkl.loads(proc_instance)
+ #   final_accumulator =processor_instance.accumulator
+    temp_acc = processor_instance.accumulator.identity()
+    data_collector = CoffeaDataCallback(temp_acc)
+
     for ds in datasets:
         job = session.createJob(ds,
                             user_params = {"dataset":ds},
-                                bulk_data = {},#XX pass compressed_proc_instance here XX
+                            bulk_data = {"processor":proc_instance},
                             callbacks = [data_collector],
                             worker_class_file="nano_worker.py")
         job.run()
-        final_accumulator = proc_instance.post_process(data_collector.coffea_accumulator)
-        for k in final_accumulator:
-            print "Output key %s = " %k
+        final_accumulator = pkl.loads(lz4f.decompress(data_collector.coffea_accumulator))
+#        final_accumulator += processor_instance.postprocess(pkl.loads(lz4f.decompress(data_collector.coffea_accumulator)))
+ 
+    print(type(final_accumulator))
 
-# Not sure if we need to allow for a more generic executor function or just automatically call striped_executor above.
-def run_dark_higgs():
-    session_name = "striped_130tb.yaml"
-    dataset = ["NanoTuples-2016_QCD_HT1500to2000_TuneCUETP8M1_13TeV-madgraphMLM-pythia8"]
-    # XX instantiate proc_instance
-    exec_function(dataset, session_name, proc_instance)
+    print("Count = ",data_collector.count)
 
+    for k in final_accumulator:
+        print("Output key = %s"%k)
+        print(final_accumulator[k])
+        print(final_accumulator[k].integrate('dataset').values(overflow='all'))
+    print("Done")
+    return final_accumulator
+
+session_name = "striped.yaml"
+dataset = ["NanoTuples-2016_QCD_HT1500to2000_TuneCUETP8M1_13TeV-madgraphMLM-pythia8"]
+
+proc_instance = AnalysisProcessor()
+proc_class = cpkl.dumps(proc_instance)
+np_array = np.frombuffer(proc_class,dtype="b")
+run_striped_job(dataset, session_name, np_array)   
